df63d19b08ef643d4c69136a71268a76bd6712b4
Somya <bansalsanjay170@gmail.com>
2021-08-31 21:25:42 +0530

    Added python code

---
+++ anagram.py
@@ -0,0 +1,15 @@
+import collections
+str1=input().lower()
+str2=input().lower()
+c1=collections.Counter(str1)
+c2=collections.Counter(str2)
+flag=0
+for c in c1:
+    print(c,c1[c])
+    if c1[c] != c2[c]:
+        flag=1
+if flag==0:
+    print("Anagram")
+else:
+    print("Not Anagram")
+
