75c373b7aed4c8184b2ac925c78f0c77c0ca2022
Somya <bansalsanjay170@gmail.com>
2021-09-01 09:53:40 +0530

    adding checkurl

---
+++ checkurl.py
@@ -0,0 +1,20 @@
+import re
+def checkValidURL(str):
+    regex = ("((http|https)://)(www.)?" +
+             "[a-zA-Z0-9@:%._\\+~#?&//=]" +
+             "{2,256}\\.[a-z]" +
+             "{2,6}\\b([-a-zA-Z0-9@:%" +
+             "._\\+~#?&//=]*)")
+    p = re.compile(regex)
+    if (str == None):
+        return False
+    if(re.search(p, str)):
+        return True
+    else:
+        return False
+url = "https://www.somya bansal.org"
+ 
+if(checkValidURL(url) == True):
+    print("Yes")
+else:
+    print("No")
