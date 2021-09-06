f1b7882f131a47173a872bd758fab2fb3b859e88
Somya <bansalsanjay170@gmail.com>
2021-09-06 22:21:35 +0530

    Revert "checking git reset"
    This reverts commit f06b35103a174d10558e879cd307f28fa25f2583.

--- anagram.py
+++ anagram.py
@@ -4,7 +4,7 @@ str2=input().lower()
 c1=collections.Counter(str1)
 c2=collections.Counter(str2)
 flag=0 #flag variable
-for c in c1:#for loop
+for c in c1:
     print(c,c1[c])
     if c1[c] != c2[c]:
         flag=1
